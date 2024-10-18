5544742461bb502df5f3a78131fffbb546c732c4
Никита Балашов <n.a.balashov@stud.mospolytech.ru>
2024-10-18 17:59:07 +0300

    lab4.1

---
+++ ConsoleApplication4/ConsoleApplication4.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+class Weapon
+{
+public:
+	int damage;
+	float weight;
+	string name;
+
+	Weapon(string name, float weight, int damage) : name(name), damage(damage), weight(weight) {}
+
+	Weapon() : name("Vandal"), damage(150), weight(15) {}
+
+};
+
+
+int main()
+{
+	Weapon weapon1("Axe", 30, 23);
+	Weapon weapon2;
+	
+	cout << "Name of weapon 1 - " << weapon1.name << ", Damage = " << weapon1.damage << ", Weight = " << weapon1.weight << endl;
+	cout << "Name of weapon 2 - " << weapon2.name << ", Damage = " << weapon2.damage << ", Weight = " << weapon2.weight << endl;
+	
+
+}
